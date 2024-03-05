d00163d78b037a744d9e544977252b52518a33ba
santiago gaviria <santiago gaviria@LAPTOP-3E9RS9UG>
2024-03-04 23:31:40 -0500

    sout

--- src/java100/Java100.java
+++ src/java100/Java100.java
@@ -6,9 +6,8 @@ public class Java100 {
 
     public static void main(String[] args) {
         System.out.println("Hola mucho gusto");
-        System.out.println("Hola ");
-         System.out.println("Hola mucho gusto 2");
-         System.out.println("Hola mucho gusto 3");
+      
+         System.out.println("hola putas");
     }
     
 }
